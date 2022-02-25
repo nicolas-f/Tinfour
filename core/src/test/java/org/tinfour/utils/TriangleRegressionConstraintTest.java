61832a981f755f373a971eb0aecfee771a4af58a
nicolas-f <github_account@nettrader.fr>
2022-02-25 15:34:12 +0100

    add unit test for unlimited loop of tinfour

--- core/src/test/java/org/tinfour/utils/TriangleRegressionConstraintTest.java
+++ core/src/test/java/org/tinfour/utils/TriangleRegressionConstraintTest.java
@@ -40,6 +40,7 @@ package org.tinfour.utils;
 
 import org.junit.Test;
 import org.tinfour.common.IConstraint;
+import org.tinfour.common.LinearConstraint;
 import org.tinfour.common.PolygonConstraint;
 import org.tinfour.common.SimpleTriangle;
 import org.tinfour.common.Vertex;
@@ -72,6 +73,125 @@ public class TriangleRegressionConstraintTest {
         }
     }
 
+    public void dumpDataClass() {
+        List<Vertex> points = new ArrayList<>();
+        List<IConstraint> constraints = new ArrayList<>();
+        String dumpFolder = "";
+        try {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(new File(dumpFolder, "tinfour_data.dump")))) {
+                writer.write("Vertex " + points.size() + "\n");
+                int index = 0;
+                for(Vertex vObj : points) {
+                    final Vertex v = (Vertex)vObj;
+                    v.setIndex(index++);
+                    writer.write(String.format(Locale.ROOT, "%d %d %d\n", Double.doubleToLongBits(v.getX()),
+                            Double.doubleToLongBits(v.getY()),
+                            Double.doubleToLongBits(v.getZ())));
+                }
+                writer.write("Constraints " + constraints.size() + " \n");
+                for (IConstraint constraint : constraints) {
+                    if (constraint instanceof LinearConstraint) {
+                        writer.write("LinearConstraint");
+                        List<Vertex> vertices = constraint.getVertices();
+                        for (final Vertex v : vertices) {
+                            writer.write(" " + v.getIndex());
+                        }
+                        writer.write("\n");
+                    } else if (constraint instanceof PolygonConstraint) {
+                        List<Vertex> vertices = constraint.getVertices();
+                        if(vertices != null && vertices.size() >= 3) {
+                            writer.write("PolygonConstraint " + constraint.getConstraintIndex());
+                            for (final Vertex v : vertices) {
+                                writer.write(" " + v.getIndex());
+                            }
+                            writer.write("\n");
+                        } else {
+                            System.out.println("Weird null polygon " + constraint);
+                        }
+                    }
+                }
+            }
+        }  catch (IOException ioEx) {
+            // ignore
+        }
+    }
+
+    @Test
+    public void testUnlimitedLoop() throws IOException {
+        String line = "";
+        try(BufferedReader reader = new BufferedReader(
+                new InputStreamReader(Objects.requireNonNull(
+                        TriangleRegressionConstraintTest.class.getResourceAsStream("unlimitedloop.dump"))))) {
+
+            // Triangulate
+            IncrementalTin tin = new IncrementalTin();
+
+            List<Vertex> meshPoints;
+
+            // read number of expected points
+            while(!line.startsWith("Vertex ")) {
+                line = reader.readLine();
+            }
+            int numberOfVertex = Integer.parseInt(line.substring(line.indexOf(" ") + 1).trim());
+            meshPoints = new ArrayList<>(numberOfVertex);
+            // Read points
+            for(int idVertex = 0; idVertex < numberOfVertex; idVertex++) {
+                line = reader.readLine();
+                StringTokenizer t = new StringTokenizer(line, " ");
+                double v0 = Double.longBitsToDouble(Long.parseLong(t.nextToken()));
+                double v1 = Double.longBitsToDouble(Long.parseLong(t.nextToken()));
+                double v2 = Double.longBitsToDouble(Long.parseLong(t.nextToken()));
+                Vertex vertex = new Vertex(v0, v1, v2);
+                meshPoints.add(vertex);
+            }
+            tin.add(meshPoints, null);
+            // Read constraints
+            while(!line.startsWith("Constraints ")) {
+                line = reader.readLine();
+            }
+            int numberOfConstraints = Integer.parseInt(line.substring(line.indexOf(" ") + 1).trim());
+            List<IConstraint> constraints = new ArrayList<>();
+            for(int idConstraint = 0; idConstraint < numberOfConstraints; idConstraint++) {
+                line = reader.readLine();
+                if(line != null && line.contains(" ")) {
+                    StringTokenizer t = new StringTokenizer(line, " ");
+                    String constraintClass = t.nextToken();
+                    int constraintIndex = Integer.parseInt(t.nextToken().trim());
+                    if (constraintClass.equals("PolygonConstraint")) {
+                        List<Vertex> vertices = new ArrayList<>();
+                        while (t.hasMoreElements()) {
+                            vertices.add(meshPoints.get(Integer.parseInt(t.nextToken())));
+                        }
+                        PolygonConstraint p = new PolygonConstraint(vertices);
+                        constraints.add(p);
+                        p.setConstraintIndex(tin, constraintIndex);
+                    } else  if (constraintClass.equals("LinearConstraint")) {
+                        List<Vertex> vertices = new ArrayList<>();
+                        while (t.hasMoreElements()) {
+                            vertices.add(meshPoints.get(Integer.parseInt(t.nextToken())));
+                        }
+                        LinearConstraint p = new LinearConstraint(vertices);
+                        constraints.add(p);
+                        p.setConstraintIndex(tin, constraintIndex);
+                    }
+                }
+            }
+            tin.addConstraints(constraints, false);
+            Map<Integer, Integer> constraintIndexToTriangleCount = new HashMap<>();
+
+            int total = 0;
+            for(SimpleTriangle t : tin.triangles()) {
+                IConstraint c = t.getContainingRegion();
+                if(c != null) {
+                    constraintIndexToTriangleCount.merge(c.getConstraintIndex(), 1, Integer::sum);
+                } else {
+                    constraintIndexToTriangleCount.merge(-1, 1, Integer::sum);
+                }
+                total+=1;
+            }
+            exportTinAsWKT(new File("target", "tinfour_wkt_triangles.csv").getPath(), tin);
+        }
+    }
     @Test
     public void testIllegalPolygon() throws IOException {
         String line = "";
